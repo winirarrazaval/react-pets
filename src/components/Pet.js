013a2fbf90d30985de53db28c9ad62e0042d97a8
Kari Bancroft <kariabancroft@gmail.com>
2018-06-07 09:18:58 -0700

    add starter code files

---
+++ src/components/Pet.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Pet extends Component {
+  
+}
+
+export default Pet;
---
+++ src/components/PetCollection.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Pet from './Pet';
+import NewPetForm from './NewPetForm';
+
+class PetCollection extends Component {
+
+}
+
+export default PetCollection;
