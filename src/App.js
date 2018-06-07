05e961cc45e02fb466edca94c7acced694875faf
Kari Bancroft <kariabancroft@gmail.com>
2018-06-07 10:04:35 -0700

    phase 1 solution for pet collection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
 import './App.css';
+import PetCollection from './components/PetCollection';
 
 class App extends Component {
   render() {
+    const pets = [
+      {
+        name: 'Horace',
+        breed: 'Pug',
+        age: 12,
+        about: ''
+      },
+      {
+        name: 'Stanley',
+        breed: 'Springer Spaniel',
+        age: 4,
+        about: ''
+      }
+    ];
+
     return (
       <div className="App">
-      {/* fill it in!!! */}
+        <h1>Pets!</h1>
+        <p>Welcome to our pets collection. Take a look around to meet our favorite furry friends. </p>
+        <PetCollection pets={ pets }/>
       </div>
     );
   }
--- src/components/PetCollection.js
+++ src/components/PetCollection.js
@@ -4,7 +4,50 @@ import Pet from './Pet';
 import NewPetForm from './NewPetForm';
 
 class PetCollection extends Component {
+  static propTypes = {
+    pets: PropTypes.array.isRequired
+  };
 
+  constructor(props) {
+    super();
+
+    this.state = { pets: props.pets };
+  }
+
+  addPet = (petInfo) => {
+    let updatedPets = this.state.pets;
+    updatedPets.push(petInfo);
+    this.setState({ pets: updatedPets});
+  }
+
+  render() {
+    const petComponents = this.state.pets.map((pet, index) => {
+      return <Pet key={ index }
+        name={ pet.name }
+        breed={ pet.breed }
+        age={ pet.age }
+        about={ pet.about }
+        />
+    });
+
+    return (
+      <section className="pets">
+        <table>
+          <tbody>
+            <tr>
+              <th>Name</th>
+              <th>Breed</th>
+              <th>Age</th>
+              <th>About</th>
+            </tr>
+            { petComponents }
+          </tbody>
+        </table>
+
+        <NewPetForm addPetCallback = { this.addPet } />
+      </section>
+    );
+  }
 }
 
 export default PetCollection;
